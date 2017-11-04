ad388ad04bd2d60c014409ce54824b60b6b76d0b
Hanna Lee <hannabeasiam@gmail.com>
2017-11-04 17:07:44 -0400

    modified html

--- lab4/script.js
+++ lab4/script.js
@@ -1,55 +1,46 @@
 
-var theMonths = new Array('January','February','March',
-'April','May','June','July','August','September',
-  'October','November','December');
-var weekDays = new Array('Sunday','Monday','Tuesday',
-  'Wednesday','Thursday','Friday','Saturday');
+var theMonths = new Array('January','February','March','April','May','June','July','August','September',  'October','November','December');
+var weekDays = new Array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+
 //get current Date and store value here
 var currentDate = new Date();
-document.write(currentDate + '<br/>');
-//get current month as number
-document.write('Today is ' + currentDate.getMonth() + '<br/>');
-//store value under thisMonth
+
+//store value under thisMonth, thisDay, thisDate
 var thisMonth = currentDate.getMonth();
-
-
 var thisDay = currentDate.getDay();
-
 var thisDate = currentDate.getDate();
-
-//get current year as number
-//document.write('4 Current Year is ' + currentDate.getYear() + '<br/>');
-
-//use getFullYear, display year correctly
+//use getFullYear
 var thisYear = currentDate.getFullYear();
 
-//document.write('here test ' + thisMonth + ' ' + thisDay + ' ' + thisDate + ' ' + thisYear);
 console.log(thisMonth);
+function calculate(){
+  //make new object nextBirthDay
+  var birthMonth = Number(prompt('What is your birth Month?'));
+  var birthDate = Number(prompt('What is your birth Date?'));
+  console.log(birthMonth);
+  var nextBirthDay = new Date();
+  var commingBD = nextBirthDay.setDate(birthDate);
+  nextBirthDay.setMonth(birthMonth - 1);
+    //nextBirthDay.setTime(0,0,0,0);
+    if ((thisMonth >= birthMonth) && (thisDate > birthDate)){
+      nextBirthDay.setYear(nextBirthDay.getFullYear()+1);
+    }
+    else if (thisMonth < birthMonth) {
+      nextBirthDay.setYear(nextBirthDay.getFullYear());
+    }
+    else if ((thismonth == birthDate) && (thisDate == birthDate)){
+      alert('Happy birthday');
+    }
 
+  document.getElementById("display1").innerHTML = 'Today is : ' + currentDate + '<br/>';
+  //document.write('Today is : ' + currentDate + '<br/>');
 
-//make new object nextBirthDay
-var birthMonth = Number(prompt('What is your birth Month?'));
-var birthDate = Number(prompt('What is your birth Date?'));
-console.log(birthMonth);
-var nextBirthDay = new Date();
-var commingBD = nextBirthDay.setDate(birthDate);
-nextBirthDay.setMonth(birthMonth - 1);
-//nextBirthDay.setTime(0,0,0,0);
-if ((thisMonth >= birthMonth) && (thisDate > birthDate)){
-  nextBirthDay.setYear(nextBirthDay.getFullYear()+1);
+  //document.write('Birth day next year is ' + nextBirthDay + '<br/>');
+  document.getElementById("display2").innerHTML = 'Birth day next year is ' + nextBirthDay;
+
+  //var diffTime = Math.abs(currentDate.getTime() - nextBirthDay.getTime());
+  var diffDay = Math.round((Number(nextBirthDay) - Number(currentDate)) / (1000 * 3600 * 24)) -1 ;
+  console.log(diffDay);
+  document.getElementById("display3").innerHTML = diffDay;
+
 }
-else if (thisMonth < birthMonth) {
-  nextBirthDay.setYear(nextBirthDay.getFullYear());
-}
-else if ((thismonth == birthDate) && (thisDate == birthDate)){
-  alert('Happy birthday');
-}
-
-document.write('Birth day next year is ' + nextBirthDay + '<br/>');
-
-
-//var diffTime = Math.abs(currentDate.getTime() - nextBirthDay.getTime());
-var diffDay = Math.round((Number(nextBirthDay) - Number(currentDate)) / (1000 * 3600 * 24)) -1 ;
-console.log(diffDay);
-
-document.write('Till your next birthday, Days left: ' + diffDay);
